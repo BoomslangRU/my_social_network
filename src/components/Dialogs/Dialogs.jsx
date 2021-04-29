b5c7f0116c1b63117f4069509b3f05a6d13f8a86
BoomslangRU <eilinals@mail.ru>
2021-04-29 22:49:57 +0300

    work in the folder Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,32 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItem}>
+                <div className={s.dialog}>
+                    Alexander
+                </div>
+                <div className={s.dialog}>
+                    Anna
+                </div>
+                <div className={s.dialog}>
+                    Ivan
+                </div>
+                <div className={s.dialog}>
+                    Oleg
+                </div>
+                <div className={s.dialog}>
+                    Dmitriy
+                </div>
+                <div className={s.dialog}>
+                    Ekaterina
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
