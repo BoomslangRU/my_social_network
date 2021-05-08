095f7e08beedc660685ca23d55905a112d94860b
BoomslangRU <eilinals@mail.ru>
2021-05-08 12:56:54 +0300

    removing unnecessary from file render

--- src/render.js
+++ src/render.js
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 import { addPost } from './redux/state'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -11,8 +10,3 @@ export let renderEntireTree = (state) => {
     </BrowserRouter>,
     document.getElementById('root'))
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
