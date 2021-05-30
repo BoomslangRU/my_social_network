7cf0c65e5436e3dfad2cd5c9e7438840a0259bf7
BoomslangRU <eilinals@mail.ru>
2021-05-30 18:40:48 +0300

    class component (extends React.Component)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from './Header.module.css'
 const Header = () => {
     return (
         <header className = {s.header}>
-            <img src='https://im0-tub-ru.yandex.net/i?id=4d208926d4ffe1c161b72d77523df8b7&n=13'></img>
+            <img src='http://cdn.onlinewebfonts.com/svg/download_534424.png'></img>
         </header>
     )
 }
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,41 +1,45 @@
 import * as axios from 'axios'
 import s from './Users.module.css'
 import userPhoto from '../../assets/images/users.png'
+import React from 'react'
 
-const Users = (props) => {
-    if (props.users.length === 0) {
+class Users extends React.Component {
+    constructor(props) {
+        super(props)
+
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
-                props.setUsers(response.data.items)
+                this.props.setUsers(response.data.items)
             })
     }
-
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
-                <div className={s.usersItem}>
-                    <div className={s.avatarItem}>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto}
-                            className={s.image} />
-                        <div className={s.buttonItem}>
-                            {u.followed
-                                ? <button className={s.button_follow} onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+    render = () => {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.id}>
+                    <div className={s.usersItem}>
+                        <div className={s.avatarItem}>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto}
+                                className={s.image} />
+                            <div className={s.buttonItem}>
+                                {u.followed
+                                    ? <button className={s.button_follow} onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                                    : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+                            </div>
+                        </div>
+                        <div className={s.infoItem}>
+                            <div className={s.name}>{u.name}</div>
+                            <div>{u.status}</div>
+                        </div>
+                        <div className={s.locationItem}>
+                            <div>{'u.country'}</div>
+                            <div>{'u.city'}</div>
                         </div>
                     </div>
-                    <div className={s.infoItem}>
-                        <div className={s.name}>{u.name}</div>
-                        <div>{u.status}</div>
-                    </div>
-                    <div className={s.locationItem}>
-                        <div>{'u.location.country'}</div>
-                        <div>{'u.location.city'}</div>
-                    </div>
                 </div>
-            </div>
-            )
-        }
-    </div>
+                )
+            }
+        </div>
+    }
 }
 
 export default Users
