c53dcfccd4edc95827124a20d97d0e10eba04960
BoomslangRU <eilinals@mail.ru>
2021-06-22 20:25:17 +0300

    working with functionality and style of forms

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,7 +5,14 @@ const onSubmit = (e) => {
     // debugger
 }
 const validate = (e) => {
-    // debugger
+    const errors = {}
+    if (e.login && (e.login.length < 3 || e.login.length > 20)) {
+        errors.login = 'login must be from 3 to 20 characters'
+    }
+    if (e.password && (e.password.length < 8 || e.password.length > 12)) {
+        errors.password = 'password must be from 8 to 12 characters'
+    }
+    return errors
 }
 
 const Login = () => (
@@ -17,16 +24,30 @@ const Login = () => (
                 <div className={s.loginItems}>
                     <h2>Login</h2>
                     <div className={s.formRow}>
-                        <Field name='login' component='input' placeholder='Login' />
+                        <Field name='login'
+                            render={({ input, meta }) => (
+                                <div>
+                                    <input {...input} placeholder='Login' />
+                                    {meta.touched && meta.error && <div className={s.error}>{meta.error}</div>}
+                                </div>
+                            )}
+                        />
                     </div>
                     <div className={s.formRow}>
-                        <Field name='password' component='input' placeholder='Password' />
+                    <Field name='password'
+                            render={({ input, meta }) => (
+                                <div>
+                                    <input {...input} placeholder='Password' />
+                                    {meta.touched && meta.error && <div className={s.error}>{meta.error}</div>}
+                                </div>
+                            )}
+                        />
                     </div>
                     <div className={s.formRow}>
                         <Field name='rememberMe' component='input' type='checkbox' /> <p>remember me</p>
                     </div>
                     <div className={s.formRow}>
-                    <button className={s.formRow} type='submit'>Login</button>
+                        <button className={s.formRow} type='submit'>Login</button>
                     </div>
                 </div>
             </form>
--- src/components/Profile/MyPosts/PostForm/PostForm.jsx
+++ src/components/Profile/MyPosts/PostForm/PostForm.jsx
@@ -2,32 +2,45 @@ import { Form, Field } from 'react-final-form'
 import s from './PostForm.module.css'
 
 const PostForm = (props) => {
-    const onSubmit = (e) => {
+  const onSubmit = (e) => {
+    if (e.newPostText) {
       props.onAddPost(e.newPostText)
     }
-    const validate = (e) => {
-      // debugger
+  }
+  const validate = (e) => {
+    const errors = {}
+    if (e.newPostText && e.newPostText.length > 100) {
+      errors.newPostText = 'post must not exceed 100 characters'
     }
-  
-    return (
-      <Form
-        onSubmit={onSubmit}
-        validate={validate}
-        render={({ handleSubmit }) => (
-          <form className={s.railway} onSubmit={handleSubmit}>
-            <div className={s.textarea}>
-              <h3>My posts</h3>
-              <div className={s.postBlock}>
-                <Field name='newPostText' component='textarea' />
-              </div>
-              <div className={s.postBlock}>
-                <button className={s.formRow} type='submit'>Add Post</button>
-              </div>
-            </div>
-          </form>
-        )}
-      />
-    )
+    return errors
   }
 
-  export default PostForm
+  return (
+    <Form
+      onSubmit={onSubmit}
+      validate={validate}
+      render={({ handleSubmit }) => (
+        <form className={s.railway} onSubmit={handleSubmit}>
+          <div className={s.textarea}>
+            <h3>My posts</h3>
+            <div className={s.postBlock}>
+              <Field name='newPostText'
+                render={({ input, meta }) => (
+                  <div>
+                    <textarea {...input} />
+                    {meta.error && <div className={s.error}>{meta.error}</div>}
+                  </div>
+                )}
+              />
+            </div>
+            <div className={s.postBlock}>
+              <button type='submit'><span>Add Post</span></button>
+            </div>
+          </div>
+        </form>
+      )}
+    />
+  )
+}
+
+export default PostForm
