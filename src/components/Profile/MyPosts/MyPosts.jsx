70d0e48524ac09643fdcc05f627ba4887bca5699
BoomslangRU <eilinals@mail.ru>
2021-07-03 14:05:28 +0300

    React.memo

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,20 +1,23 @@
+import { memo } from 'react'
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
 import PostForm from './PostForm/PostForm'
 
-const MyPosts = (props) => {
+// React.memo in this version it does not make sense
+// and additions for educational purposes
+const MyPosts = memo(props => {
 
-    let postsElements = props.posts.map(p => <Post message={p.message} key={p.key} likeCounter={p.likeCounter} />)
+  let postsElements = props.posts.map(p => <Post message={p.message} key={p.key} likeCounter={p.likeCounter} />)
 
-    return (
-      <div className={s.item}>
-        <div className={s.postBlock}>
-          <PostForm onAddPost={props.onAddPost} />
-        </div >
-        {postsElements}
-      </div>
-    )
-  }
+  return (
+    <div className={s.item}>
+      <div className={s.postBlock}>
+        <PostForm onAddPost={props.onAddPost} />
+      </div >
+      {postsElements}
+    </div>
+  )
+})
 
 export default MyPosts
 
